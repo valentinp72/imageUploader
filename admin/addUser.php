34bdf9a162bd300bb9566acf0c3abbb92b66e629
Valentin Pelloin <valentinpelloin@gmail.com>
2017-05-21 22:43:26 +0200

    better adduser

--- admin/addUser.php
+++ admin/addUser.php
@@ -1,5 +1,8 @@
 <?php
 
+	$title = "Add an user";
+	$page  = "addUser";
+
 	require('header.php');
 
 	if(
@@ -9,8 +12,9 @@
 		!empty($_POST['password2']) and
 		$_POST['password'] == $_POST['password2']
 	){
-		echo "Write this into the users.php file :<br/>$";
-		echo "users = array('" . $_POST['login'] . "' => '" . password_hash($_POST['password'], PASSWORD_DEFAULT) . "');";
+		echo "<strong>1 - </strong>Write this into the users.php file :<br/>";
+		echo "<pre>\$users = array('" . $_POST['login'] . "' => '" . password_hash($_POST['password'], PASSWORD_DEFAULT) . "');</pre>";
+		echo "<strong>2 - </strong>Now, you can <a href='auth.php'>log in</a>.";
 	}
 	else {
 
@@ -18,12 +22,28 @@
 			echo "Passwords does not match.";
 	?>
 
-	<form method="post">
-		Login : <input name="login" required/><br/>
-		Password : <input type="password" name="password" required/><br/>
-		Retype password : <input type="password" name="password2" required/><br/>
-		<input type="submit" name="submit" />
-	</form>
+
+	<div class="center-container">
+		<h1>Add an user</h1>
+	</div>
+	<div class="center-container">
+
+		<form method="post">
+			<div class="form-group">
+				<label for="login">Login:</label>
+				<input name="login" required class="form-control">
+			</div>
+			<div class="form-group">
+				<label for="password">Password:</label>
+				<input type="password" name="password" required class="form-control">
+			</div>
+			<div class="form-group">
+				<label for="password">Retype password:</label>
+				<input type="password" name="password2" required class="form-control">
+			</div>
+			<input type="submit" name="submit" value="Submit" class="btn btn-default"/>
+		</form>
+	</div>
 
 	<?php
 
